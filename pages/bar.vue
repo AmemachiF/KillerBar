4d07c7db15d653f8fddd93c75a164b5cf44d4f5e
Southseast <i@southsea.st>
2021-01-20 17:00:26 +0800

    update voiceCard

--- pages/bar.vue
+++ pages/bar.vue
@@ -6,39 +6,29 @@
 
 <script lang="ts">
 import Vue from 'vue'
-import { Voice } from '~/components/VoiceCard.vue'
 
 export default Vue.extend({
   data () {
-    const audio: Voice[] = [
-      {
-        text: '一键mua',
-        src: 'http://amemachif.com/static/media/mua.a1eaf3d4.aac'
-      },
-      {
-        text: '一键认爹',
-        src: 'http://amemachif.com/static/media/jiaobaba.60c344c1.aac'
-      },
-      {
-        text: '一键吸脑',
-        src: 'http://amemachif.com/static/media/xinao.5cfb2538.aac'
-      },
-      {
-        text: '一键牛逼',
-        src: 'http://amemachif.com/static/media/niubi.cd4c14f5.aac'
-      },
-      {
-        text: '一键恶心',
-        src: 'http://amemachif.com/static/media/exin.1499d557.aac'
-      },
-      {
-        text: '一键很会',
-        src: 'http://amemachif.com/static/media/henhui.7e5b63b5.aac'
-      }
-    ]
-
     return {
-      audio
+      audio: {}
+    }
+  },
+  mounted () {
+    this.fetchAudio()
+  },
+  methods: {
+    fetchAudio () {
+      this.$axios.get('https://api.amemachif.com:2333/audio')
+        .then((res) => {
+          if (res.data.code === 20000) {
+            this.audio = res.data.data
+          } else {
+            this.audio = {}
+          }
+        })
+        .catch((_) => {
+          this.audio = {}
+        })
     }
   }
 })
