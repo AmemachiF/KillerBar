39bbdfe461337c85def8256ac36f41ac6a178a3f
Southseast <i@southsea.st>
2021-01-28 16:14:55 +0800

    add stupid game

---
+++ components/GameCard.vue
@@ -0,0 +1,36 @@
+<template>
+  <b-card>
+    <b-card-text>
+      <iframe id="game" :style="gameSize" frameborder="0" src="./game_src/index.html"/>
+    </b-card-text>
+  </b-card>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+
+  data () {
+    return {
+      gameSize: {
+        width: '',
+        height: ''
+      }
+    }
+  },
+  created () {
+    this.changeGameSize()
+  },
+  methods: {
+    changeGameSize () {
+      this.gameSize.width = global.innerWidth - 35 + 'px'
+      this.gameSize.height = (global.innerWidth - 35) / 400 * 700 + 'px'
+    }
+  }
+})
+</script>
+<style>
+.card-body{
+  padding: 0;
+}
+</style>
