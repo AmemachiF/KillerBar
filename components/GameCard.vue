fb2452fd7e20c941d8bb7a5df1bee71fce4c57b6
Chang Feng <chang_196700@hotmail.com>
2021-01-30 19:55:46 +0800

    feat: new game version
    Signed-off-by: Chang Feng <chang_196700@hotmail.com>

--- components/GameCard.vue
+++ components/GameCard.vue
@@ -1,8 +1,18 @@
 <template>
   <b-card class="game-aspect">
+    <template #header>
+      <span>
+        {{ !newVersion ? '原版' : '重制版（开发中）' }}
+      </span>
+      <span class="d-inline float-right">
+        <button @click="newVersion = !newVersion">
+          切换{{ newVersion ? '原版' : '重制版（开发中）' }}
+        </button>
+      </span>
+    </template>
     <b-card-text>
       <b-aspect aspect="4:7">
-        <iframe id="game" frameborder="0" src="./synthetic_game/index.html" />
+        <iframe id="game" frameborder="0" :src="`./synthetic_game${newVersion ? '' : '_old'}/index.html`" />
       </b-aspect>
     </b-card-text>
   </b-card>
@@ -11,6 +21,11 @@
 <script lang="ts">
 import Vue from 'vue'
 export default Vue.extend({
+  data () {
+    return {
+      newVersion: false
+    }
+  }
 })
 </script>
 <style scoped>
@@ -24,7 +39,7 @@ export default Vue.extend({
 
 .game-aspect, #game {
   height: 100%;
-  width: calc((100vh - 150px) / 7 * 4);
+  width: calc((100vh - 200px) / 7 * 4);
   max-width: 100%;
 }
 </style>
